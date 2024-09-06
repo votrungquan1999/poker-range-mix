5d853bedf7a4f94bdc7b38c9cdd0fe2b97d4c2c1
Quan Vo <votrungquan99@gmail.com>
2024-09-06 23:27:55 +0700

    feat: connect to mongodb with name

--- src/server/mongodb/mongodb.ts
+++ src/server/mongodb/mongodb.ts
@@ -1,23 +1,24 @@
-import { MongoClient } from 'mongodb'
-import setup from '../asynclocal'
-import { memoize } from 'lodash'
+import { MongoClient } from "mongodb";
+import setup from "../asynclocal";
+import { memoize } from "lodash";
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017'
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const MONGODB_DB = process.env.MONGODB_DB || "poker_range";
 
 async function newMongoDB() {
-  const client = MongoClient.connect(MONGODB_URI)
+	const client = MongoClient.connect(MONGODB_URI);
 
-  return client
+	return client;
 }
 
-const asyncContext = setup(memoize(newMongoDB))
+const asyncContext = setup(memoize(newMongoDB));
 
 export function getMongoDb() {
-  return asyncContext.get().db()
+	return asyncContext.get().db(MONGODB_DB);
 }
 
 export function getMongoDbClient() {
-  return asyncContext.get()
+	return asyncContext.get();
 }
 
-export const injectMongoDB = asyncContext.inject
+export const injectMongoDB = asyncContext.inject;
